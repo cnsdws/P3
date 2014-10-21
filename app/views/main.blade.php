0c8d01357736c136e3e927c91dd1bae53e7b6c3e
Dennis Stevens <cnsdws@yahoo.com>
2014-10-21 13:19:41 -0600

    format changes, deleted sections to pass W3 validation

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,24 +1,23 @@
 @extends('_master')
 
 @section('css')
-<link href="../css/bootstrap.css" rel="stylesheet">
+<link href="../public/css/bootstrap.css" rel="stylesheet">
 @stop
 
-<!-- Main page section for the Lorem text generator -->
 @section('Lorem')
-
+<!-- Main page section for the Lorem text generator -->
 <h1 class="media-heading">Project 3 - Lorem Ipsum and Faux User Generator</h1>
     <h2>Project 3</h2>
    		
    		<h3>Lorem Ipsum Generator</h3>
-		    <section>
+		    <p>
 		    	This tool will generate paragraphs of filler text in pseudo-Latin (Lorem Ipsum) for 
 		    	your use. 
-		    </section>
-		    <section>
+		    </p>
+		    <p>
 		    	<h4> <a href="http://p3.dennisstevens.me/lorem">Generate Lorem</a> </h4>
-		    </section>
-		    <section>
+		    </p>
+		    <p>
 		    	Here's a sample:
 		        
 				<?php
@@ -26,21 +25,22 @@
 					$paragraphs = $generator->getParagraphs(1);
 					echo '<p>'.implode('<p>', $paragraphs).'<br>';
 				?> 
-			</section>
+			</p>
 			
 @stop
 <!-- Main page section for the user generator -->
 @section('Users')
 
 		<h3>User Generator</h3>
-		    <section>
+		    <p>
 		    	This tool will generate pseudo user information so you can populate databases or whatever.
-		    </section>
-		    <section>
+		    </p>
+		    <p>
 		    	<h4> <a href="http://p3.dennisstevens.me/users">Generate Users</a> </h4>
-		    </section>
-		    <section>
-		    	<p>Here's an example:</p>
+		    </p>
+		    <p>
+		    	Here's an example:
+		    	<br>
 
 		    	<?php
 					
@@ -57,6 +57,6 @@
 					echo 'Email: '.$faker->email;
 					echo '<br>'; 
 				?>
-			</section>
+			</p>
 
 @stop
