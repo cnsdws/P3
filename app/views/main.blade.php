4a6c8af1332320ba8053e9b39f7ae80ad5308876
Dennis Stevens <cnsdws@yahoo.com>
2014-10-21 13:27:40 -0600

    fixed html tags

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -5,58 +5,59 @@
 @stop
 
 @section('Lorem')
-<!-- Main page section for the Lorem text generator -->
+
 <h1 class="media-heading">Project 3 - Lorem Ipsum and Faux User Generator</h1>
-    <h2>Project 3</h2>
-   		
-   		<h3>Lorem Ipsum Generator</h3>
-		    <p>
-		    	This tool will generate paragraphs of filler text in pseudo-Latin (Lorem Ipsum) for 
-		    	your use. 
-		    </p>
-		    <p>
-		    	<h4> <a href="http://p3.dennisstevens.me/lorem">Generate Lorem</a> </h4>
-		    </p>
-		    <p>
-		    	Here's a sample:
+    <!-- Main page section for the Lorem text generator -->
+   	<h3>Lorem Ipsum Generator</h3>
+	    <p>
+	   	This tool will generate paragraphs of filler text in pseudo-Latin (Lorem Ipsum) for 
+	   	your use. 
+	    </p>
+	
+	    <p>
+	   	<h4> <a href="http://p3.dennisstevens.me/lorem">Generate Lorem</a> </h4>
+	    </p>
+	
+	    <p>
+	   	Here's a sample:
 		        
-				<?php
-					$generator = new Badcow\LoremIpsum\Generator();
-					$paragraphs = $generator->getParagraphs(1);
-					echo '<p>'.implode('<p>', $paragraphs).'<br>';
-				?> 
-			</p>
+			<?php
+				$generator = new Badcow\LoremIpsum\Generator();
+				$paragraphs = $generator->getParagraphs(1);
+				echo '<p>'.implode('<p>', $paragraphs).'<br>';
+			?> 
+		</p>
 			
 @stop
-<!-- Main page section for the user generator -->
+
 @section('Users')
+	<!-- Main page section for the user generator -->
+	<h3>User Generator</h3>
+	    <p>
+	    	This tool will generate pseudo user information so you can populate databases or whatever.
+	    </p>
+		    
+	   	<h4> <a href="http://p3.dennisstevens.me/users">Generate Users</a> </h4>
+		    
+	    <p>
+	    	Here's an example:
+			<br>
 
-		<h3>User Generator</h3>
-		    <p>
-		    	This tool will generate pseudo user information so you can populate databases or whatever.
-		    </p>
-		    <p>
-		    	<h4> <a href="http://p3.dennisstevens.me/users">Generate Users</a> </h4>
-		    </p>
-		    <p>
-		    	Here's an example:
-		    	<br>
+		   	<?php
+				
+				// use the factory to create a Faker\Generator instance
+				$faker = Faker\Factory::create();
 
-		    	<?php
-					
-					// use the factory to create a Faker\Generator instance
-					$faker = Faker\Factory::create();
-
-					// generate data by accessing properties
-					echo 'Name: '.$faker->name;
-				  	echo '<br>'; 
-				  	echo 'Address: '.$faker->address;
-					echo '<br>'; 
-					echo 'Phone: '.$faker->phoneNumber;
-					echo '<br>';
-					echo 'Email: '.$faker->email;
-					echo '<br>'; 
-				?>
-			</p>
+				// generate data by accessing properties
+				echo 'Name: '.$faker->name;
+			  	echo '<br>'; 
+			  	echo 'Address: '.$faker->address;
+				echo '<br>'; 
+				echo 'Phone: '.$faker->phoneNumber;
+				echo '<br>';
+				echo 'Email: '.$faker->email;
+				echo '<br>'; 
+			?>
+		</p>
 
 @stop
